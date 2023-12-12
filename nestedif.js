b38c16493e28327832efc8be68634376a5f30a22
isad05 <38412396+isad05@users.noreply.github.com>
2023-12-12 14:25:49 +0800

    Create nestedif.js

---
+++ nestedif.js
@@ -0,0 +1,43 @@
+function deeplyNestedCheck(value) {
+    if (value === 1) {
+        if (value === 2) {
+            if (value === 3) {
+                if (value === 4) {
+                    if (value === 5) {
+                        if (value === 6) {
+                            if (value === 7) {
+                                if (value === 8) {
+                                    if (value === 9) {
+                                        if (value === 10) {
+                                            if (value === 11) {
+                                                if (value === 12) {
+                                                    if (value === 13) {
+                                                        if (value === 14) {
+                                                            if (value === 15) {
+                                                                if (value === 16) {
+                                                                    if (value === 17) {
+                                                                        if (value === 18) {
+                                                                            if (value === 19) {
+                                                                                if (value === 20) {
+                                                                                    console.log("Reached level 20");
+                                                                                }
+                                                                            }
+                                                                        }
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
